feb92ae1dd4e04bd54d3ec44af7ea262895e46cd
Evans <chersoftwaredev@gmail.com>
2024-12-10 20:58:36 +0300

    added react practice code

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h3>hello people</h3>
+      <ul>
+        <li>
+          <a href='#'>hello world</a>
+        </li>
+      </ul>
+      <div>
+        <h2>Welcome to react</h2>
+        <input type='text' name='' id='' />
+      </div>
+    </>
+  );
+}
+
+export default App;
